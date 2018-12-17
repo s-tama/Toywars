8bfd1b7677738da8e77a3f9c46feeb17b5179d62
TamamuraSyuuki <syuuki0105@gmail.com>
2018-12-17 10:26:44 +0900

    AIのレベル1を実装

---
+++ MyGame/Game/Strategies/AI_Level1.h
@@ -0,0 +1,50 @@
+﻿//
+// AI_Level1.h
+//
+#pragma once
+
+
+// ヘッダーファイルのインクルード -------------------------------------------------
+#include "AI.h"
+
+
+// クラスの宣言 -------------------------------------------------------------------
+namespace MyGame
+{
+	class EnemyStrategy;
+}
+
+
+// クラスの定義 -------------------------------------------------------------------
+namespace MyGame
+{
+	/// <summary>
+	/// レベル0のAIクラス
+	/// </summary>
+	class AI_Level1 : public AI
+	{
+	private:
+
+		// 方向
+		static const float DIRECTION[4];
+
+
+	public:
+
+		// コンストラクタ
+		AI_Level1(Enemy* pEnemy);
+
+		// 初期化処理
+		void Initialize() override;
+
+		// 思考
+		void Think(float elapsedTime) override;
+
+
+	private:
+
+		MyLibrary::GameObject* m_pTarget;					// ターゲットへのポインタ
+		int m_time;											// タイマー(int)
+		float m_timef;										// タイマー(float)
+	};
+}
